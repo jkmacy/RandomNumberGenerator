4aa26b71d5d55ef6291e9dba1f66b17ae9c60500
jkmacy <joyousjake.jm@gmail.com>
2016-11-10 12:31:37 -0600

    Finished basic Random Number Generator.

--- RandomNumber/src/main/Generator.java
+++ RandomNumber/src/main/Generator.java
@@ -1,10 +1,20 @@
 package main;
 
 import java.util.Scanner;
+import static java.lang.System.*;
 
 public class Generator {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(in);
+        do {
+            out.println("Hello, this is your everyday, everyman random number generator!\nLet's get started!");
+            out.print("Pick a number range (no less than 0, no greater than 1,000,000; ex. 1-25): ");
+            int a = input.nextInt(); int b = input.nextInt(); input.nextLine();
+            out.println("Computing...");
+            int rand = (int)(Math.random()*b) + a;
+            out.printf("You got %d\nAsk again? (Y/n) ", rand);
+            if (input.nextLine().toLowerCase().contains("n")) break;
+        } while(true);
     }
 }
