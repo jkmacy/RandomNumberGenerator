3a4a4104bea899b2ff4e5d555462d0db33169f7d
jkmacy <joyousjake.jm@gmail.com>
2016-11-14 12:20:08 -0600

    Modifed .gitignore. Fixed input method.

--- RandomNumber/src/main/Generator.java
+++ RandomNumber/src/main/Generator.java
@@ -2,19 +2,30 @@ package main;
 
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Generator {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(in);
-        do {
-            out.println("Hello, this is your everyday, everyman random number generator!\nLet's get started!");
+        Pattern num = Pattern.compile("[0-9]+");
+        out.println("Hello, this is your everyday, everyman random number generator!\nLet's get started!");
+        while (true) {
             out.print("Pick a number range (no less than 0, no greater than 1,000,000; ex. 1-25): ");
-            int a = input.nextInt(); int b = input.nextInt(); input.nextLine();
-            out.println("Computing...");
-            int rand = (int)(Math.random()*b) + a;
+            String range = input.nextLine();
+            Matcher matcher = num.matcher(range);
+            int a=0;
+            int b=0;
+            if (matcher.find())
+                a = Integer.parseInt(range.substring( matcher.toMatchResult().start(),matcher.toMatchResult().end() ));
+            if (matcher.find())
+                b = Integer.parseInt(range.substring( matcher.toMatchResult().start(),matcher.toMatchResult().end() ));
+            out.printf("Computing...\n");
+            int rand = (int)(Math.random()*(b-a)) + a;
             out.printf("You got %d\nAsk again? (Y/n) ", rand);
             if (input.nextLine().toLowerCase().contains("n")) break;
-        } while(true);
+            out.print("Same range? ");
+        }
     }
 }
