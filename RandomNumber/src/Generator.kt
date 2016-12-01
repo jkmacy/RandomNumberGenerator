e8acc9d26a681562ba06b678aabdb444175e350d
jkmacy <joyousjake.jm@gmail.com>
2016-11-30 18:05:33 -0600

    Can quit when choosing range.

--- RandomNumber/src/Generator.kt
+++ RandomNumber/src/Generator.kt
@@ -5,6 +5,7 @@ object Generator {
     val input = Scanner(`in`)
     var a: Int? = 0
     var b: Int? = 0
+
     @JvmStatic
     fun main(args: Array<String>) {
         val num = Regex("[0-9]+")
@@ -13,6 +14,9 @@ object Generator {
             print("Pick a number range (no less than 0, no greater than 1,000,000; ex. 1-25): ")
             val range = input.nextLine()
             val result : Sequence<MatchResult>? = num.findAll(range)
+
+            if (Regex("^[nN]").containsMatchIn(range))
+                break
 
             result?.let {
                 a = it.elementAtOrNull(0)?.value?.toInt()
